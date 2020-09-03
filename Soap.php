f35be98e6b481f26366d89e9a8e2aa6d830c2042
Alan Vinicius <alan123vinicius@gmail.com>
2020-09-02 21:59:32 -0300

    Add files via upload

---
+++ Soap.php
@@ -0,0 +1,34 @@
+<?php
+
+include ("lib/nusoap.php");
+
+class Soap
+{
+    public function Request()
+    {
+        $client = new nusoap_client("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?WSDL",true);
+        
+        $ap_param = array(
+		'nCdEmpresa'=>'',
+		'sDsSenha'=>'',
+		'nCdServico'=>'41106',
+		'sCepOrigem'=>'99010150',
+		'sCepDestino'=>'78896000',
+		'nVlPeso'=>'1',
+		'nCdFormato'=>'1',
+		'nVlAltura'=>'20',
+		'nVlLargura'=>'30',
+		'nVlDiametro'=>'40',
+		'nVlComprimento'=>'20',
+		'sCdMaoPropria'=>'S',
+		'nVlValorDeclarado'=>'200',
+		'sCdAvisoRecebimento'=>'S'
+	);
+        
+        $resultado = $client->call('CalcPrecoPrazo',$ap_param);
+        print_r($resultado);  
+    }
+}
+
+$execute = new Soap();
+$execute->Request();
